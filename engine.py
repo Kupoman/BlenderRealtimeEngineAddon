ea534d246713936186ccfc46aadad5f07c00febf
Daniel Stokes <kupomail@gmail.com>
2015-01-25 16:20:21 -0800

    Display: Rendering image data in self.display

--- engine.py
+++ engine.py
@@ -5,6 +5,7 @@ if "bpy" in locals():
 else:
     import bpy
 
+from bgl import *
 
 DEFAULT_WATCHLIST = [
     "actions",
@@ -39,6 +40,11 @@ class RealTimeEngine(bpy.types.RenderEngine):
         for collection in self._watch_list:
             collection_name = get_collection_name(collection)
             self._tracking_sets[collection_name] = set()
+
+        # Display image
+        self.width = 1
+        self.height = 1
+        self.display = Buffer(GL_BYTE, 3, [0, 0, 0])
 
         self._old_vmat = None
         self._old_pmat = None
@@ -88,6 +94,51 @@ class RealTimeEngine(bpy.types.RenderEngine):
         viewport = [region.x, region.y, region.width, region.height]
 
         self.update_view(vmat, pmat, viewport)
+
+        glGetError()
+        glPushAttrib(GL_ALL_ATTRIB_BITS)
+
+        glDisable(GL_DEPTH_TEST)
+        glDisable(GL_CULL_FACE)
+        glEnable(GL_TEXTURE_2D)
+
+        glClearColor(0, 0, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT)
+
+        glMatrixMode(GL_MODELVIEW)
+        glPushMatrix()
+        glLoadIdentity()
+        glMatrixMode(GL_PROJECTION)
+        glPushMatrix()
+        glLoadIdentity()
+
+        bgl_buffer = Buffer(GL_INT, 1)
+        glActiveTexture(GL_TEXTURE0)
+        glGenTextures(1, bgl_buffer)
+        tex = bgl_buffer[0]
+        glBindTexture(GL_TEXTURE_2D, tex)
+        width = viewport[2] - viewport[0]
+        height = viewport[3] - viewport[1]
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, self.width, self.height, 0, GL_RGB, GL_BYTE, self.display)
+
+        glBegin(GL_QUADS)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3i(-1, -1, 0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3i(1, -1, 0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3i(1, 1, 0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3i(-1, 1, 0)
+        glEnd()
+
+        glDeleteTextures(1, bgl_buffer)
+
+        glPopMatrix()
+        glMatrixMode(GL_MODELVIEW)
+        glPopMatrix()
+
+        glPopAttrib()
 
     def add_objects(self, add_set):
         for data in add_set:

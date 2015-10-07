b3331c85fd793f6d8e4cd5ec628c9c273cd34542
Daniel Stokes <kupomail@gmail.com>
2015-10-06 18:53:46 -0700

    Clean Up: Fix PyLint issues in double_buffer.py and dummy.py

--- brte/processors/double_buffer.py
+++ brte/processors/double_buffer.py
@@ -3,8 +3,12 @@ import ctypes
 
 class DoubleBuffer:
     def __init__(self, size, swap_callback):
-        self.resize(size)
+        self.read_buffer = None
+        self.write_buffer = None
+        self.size = 0
         self.swap_callback = swap_callback
+
+        self.resize(size)
 
     def resize(self, size):
         self.read_buffer = (ctypes.c_ubyte * size)(0)
--- brte/processors/dummy.py
+++ brte/processors/dummy.py
@@ -1,23 +1,31 @@
+'''An example module demonstrating the implementation of a processor'''
 import ctypes
 import math
 
 
 class DummyProcessor:
+    '''An example processor that simply changes the viewport color'''
+
     def __init__(self, buffer):
+        '''Construct a DummyProcessor with a buffer for drawing into'''
+
         self.buffer = buffer
         self.value = 0
 
     def process_data(self, data):
+        '''Accept converted data to be consumed by the processor'''
         pass
 
-    def update(self, dt):
-        self.value += dt
+    def update(self, timestep):
+        '''Advance the processor by the timestep and update the viewport image'''
+
+        self.value += timestep
         interval = 3.0
 
         while self.value > interval:
             self.value -= interval
-        t = self.value / interval
+        alpha = self.value / interval
 
-        t = 1.0 - (0.5 * math.cos(2 * math.pi * t) + 0.5)
-        ctypes.memset(self.buffer.write_buffer, int(255*t), self.buffer.size)
+        alpha = 1.0 - (0.5 * math.cos(2 * math.pi * alpha) + 0.5)
+        ctypes.memset(self.buffer.write_buffer, int(255*alpha), self.buffer.size)
         self.buffer.swap()

307240f84cbe97de379fedec3da4d8a1f343e77c
Mitchell Stokes <mogurijin@gmail.com>
2018-01-14 15:33:56 -0800

    BTFConverter: Allow gltf_settings to be set from the constructor

--- brte/converters/btf.py
+++ brte/converters/btf.py
@@ -18,24 +18,29 @@ def togl(matrix):
 
 
 class BTFConverter:
+    def __init__(self, gltf_settings=None):
+        if gltf_settings is None:
+            available_extensions = blendergltf.extension_exporters
+            gltf_settings = {
+                'images_data_storage': 'REFERENCE',
+                'nodes_export_hidden': True,
+                'images_allow_srgb': True,
+                'asset_profile': 'DESKTOP',
+                'meshes_apply_modifiers': False, # Cannot be done in a thread
+                'extension_exporters': [
+                    available_extensions.khr_materials_common.KhrMaterialsCommon(),
+                    available_extensions.blender_physics.BlenderPhysics(),
+                ],
+            }
+
+        self.gltf_settings = gltf_settings
+
     def convert(self, add_delta, update_delta, remove_delta, view_delta):
         for key, value in update_delta.items():
             if value:
                 add_delta[key] = value
 
-        available_extensions = blendergltf.extension_exporters
-        gltf_settings = {
-            'images_data_storage': 'REFERENCE',
-            'nodes_export_hidden': True,
-            'images_allow_srgb': True,
-            'asset_profile': 'DESKTOP',
-            'meshes_apply_modifiers': False, # Cannot be done in a thread
-            'extension_exporters': [
-                available_extensions.khr_materials_common.KhrMaterialsCommon(),
-                available_extensions.blender_physics.BlenderPhysics(),
-            ],
-        }
-        data = blendergltf.export_gltf(add_delta, gltf_settings)
+        data = blendergltf.export_gltf(add_delta, self.gltf_settings)
 
         if view_delta:
             self.export_view(view_delta, data)
